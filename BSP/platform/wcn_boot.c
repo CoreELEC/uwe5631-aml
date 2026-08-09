8fd077e1c2533b88babc8709c2f4ab8e28fe0b7c
Stanislav Vlasic <svlasic@gmail.com>
2026-08-09 14:35:38 +0200

    Add power on/off of the wifi chip for Amlogic boards

--- BSP/platform/wcn_boot.c
+++ BSP/platform/wcn_boot.c
@@ -63,6 +63,7 @@ extern int wifi_irq_trigger_level(void);
 extern void extern_bt_set_enable(int is_on);
 #endif
 extern void extern_wifi_set_enable(int is_on);
+extern void set_usb_wifi_power(int is_power);
 #endif
 
 #ifdef CONFIG_GOKE_BOARD
@@ -1338,7 +1339,7 @@ static int gnss_download_firmware(void) {
 	char *buf;
 	int err;
 	unsigned int load_fw_cnt = 0;
-	
+
 	buf = marlin_dev->write_buffer;
 reload:
 	WCN_DEBUG("%s %d.load from %s\n", __func__, load_fw_cnt + 1, GNSS_FIRMWARE_PATH);
@@ -2667,6 +2668,28 @@ static int chip_reset_release(int val)
 
 	return 0;
 }
+#ifdef CONFIG_AML_BOARD
+void marlin_wifi_power(bool on)
+{
+	static unsigned int chip_en_count;
+
+	if (on) {
+		if (chip_en_count == 0) {
+			set_usb_wifi_power(0);
+			set_usb_wifi_power(1);
+			WCN_INFO("marlin chip wifi power on\n");
+		}
+		chip_en_count++;
+	} else {
+		chip_en_count--;
+		if (chip_en_count == 0) {
+			set_usb_wifi_power(0);
+			WCN_INFO("marlin chip wifi power off\n");
+		}
+	}
+	return;
+}
+#endif
 void marlin_chip_en(bool enable, bool reset)
 {
 	static unsigned int chip_en_count;
@@ -3194,6 +3217,9 @@ int chip_power_on(int subsys)
 	marlin_avdd18_dcxo_enable(true);
 	marlin_clk_enable(true);
 	marlin_digital_power_enable(true);
+#ifdef CONFIG_AML_BOARD
+	marlin_wifi_power(true);
+#endif
 #ifdef CONFIG_GOKE_BOARD
 	if (subsys == 0xff) {
 		gk_gpio_set_value(RTL_REG_RST_GPIO, 0);
@@ -3252,6 +3278,9 @@ int chip_power_off(int subsys)
 	}
 #else
 	marlin_chip_en(false, false);
+#endif
+#ifdef CONFIG_AML_BOARD
+	marlin_wifi_power(false);
 #endif
 	marlin_digital_power_enable(false);
 	marlin_analog_power_enable(false);
@@ -4129,6 +4158,9 @@ static void marlin_remove(struct platform_device *pdev)
 		wifipa_enable(0);
 		pmic_bound_xtl_assert(0);
 		marlin_chip_en(false, false);
+		#ifdef CONFIG_AML_BOARD
+			marlin_wifi_power(true);
+		#endif
 	}
 	wcn_bus_deinit();
 #ifdef CONFIG_WCN_SLP
@@ -4165,6 +4197,9 @@ static void marlin_shutdown(struct platform_device *pdev)
 		wifipa_enable(0);
 		pmic_bound_xtl_assert(0);
 		marlin_chip_en(false, false);
+		#ifdef CONFIG_AML_BOARD
+			marlin_wifi_power(true);
+		#endif
 	}
 
 #if (defined(CONFIG_GOKE_BOARD) && defined(CONFIG_WCN_USB))
